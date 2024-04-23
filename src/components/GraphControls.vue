e5807b5fa525dfbd3498100fe0545ea8be6402d0
Agustín García <agustin94garcia@gmail.com>
2024-04-23 09:06:39 +0100

    Slider style

--- src/components/GraphControls.vue
+++ src/components/GraphControls.vue
@@ -8,25 +8,41 @@ const ipcGeneral = inject('ipcGeneral', { count: 0 });
 <template>
     <div class='graph__controls'>
         <input v-if="ipcGeneral.count"
-            class='dateInput'
-            type='range'
-            :min='0'
-            :max='ipcGeneral.count - 1'
-            v-model='dataIndex'
+          class='customSlider'
+          type='range'
+          :min='0'
+          :max='ipcGeneral.count - 1'
+          v-model='dataIndex'
         >
         <div v-else>...</div>
     </div>
 </template>
 
 <style scoped>
+.customSlider {
+  appearance: none;
+  overflow: hidden;
+}
+
+.customSlider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    height: 15px;
+    width: 15px;
+    background-color: #f50;
+    box-shadow: -480px 0 0 480px #f50;
+}
+
 @media screen and (max-width: 600px) {
-  .dateInput {
+  .customSlider {
     width: 100%;
+    margin-block: 20px;
+    height: 30px;
   }
 }
 
 @media screen and (min-width: 600px) {
-  .dateInput {
+  .customSlider {
     width: 50%;
   }
 }
